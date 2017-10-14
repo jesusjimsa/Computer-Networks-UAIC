27fdae80ea63a105b65df54fbe3f4c3c278fc81f
jesusjimsa <jesusjimsa@icloud.com>
2017-10-14 19:33:57 +0300

    Translated

--- Lab 2/Examples/sig.c
+++ Lab 2/Examples/sig.c
@@ -1,7 +1,7 @@
 /* sig.c
 
-   Afiseaza continutul fisierului "/etc/services" la aparitia
-   semnalului SIGUSR2
+   Muestra el contenido del archivo "/ etc / services" cuando aparece
+    la señal SIGUSR2
 
    Autor: Sabin-Corneliu Buraga <busaco@infoiasi.ro> (c)1998-2001
    Ultima actualizare: 10 iulie 2001
@@ -12,32 +12,33 @@
 #include <sys/types.h>
 #include <stdlib.h>
 
-void sighandler (int sig){		/* functia de tratare a semnalului */
+void sighandler (int sig){		/* función de procesamiento de señal*/
 	pid_t pid;
 
-	if ((pid = fork ()) < 0){	/* executam 'less' ca proces copil */
+	if ((pid = fork ()) < 0){	/* ejecutamos 'less' como un proceso secundario */
 		perror ("fork()");
 		exit (1);
 	}
-	if (!pid){			/* copil */
+	if (!pid){			/* hijo */
 		execl ("/usr/bin/less", "less", "/etc/services", NULL);
 		perror ("exec()");
 		exit (1);
 	}
-	else{				/* parinte */
-		if (wait (NULL) < 0)
+	else{				/* padre */
+		if (wait (NULL) < 0){
 			perror ("wait()");
+		}
 	}
 }
 
-int main (){				/* programul principal */
-	/* atasam functia de tratare la semnalul SIGUSR2 */
+int main (){	/* programa principal */
+	/* adjuntamos la función de tratamiento a la señal SIGUSR2 */
 	if (signal (SIGUSR2, sighandler) == SIG_ERR){
 		perror ("signal()");
 		return 1;
 	}
 	
-	/* asteptam aparitia unui semnal */
+	/* estamos esperando que aparezca una señal */
 	pause ();
 	
 	return 0;
