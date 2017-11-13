bf6821ad571586fdce8bd5910283f4c882cc190f
jesusjimsa <jesusjimsa@icloud.com>
2017-11-14 01:32:09 +0200

    Translate to Spanish

---
+++ Lab 6/ex1Client.c
@@ -0,0 +1,93 @@
+/*
+	Write a client and a TCP server following the steps described in the lab, where:
+		- the client sends two numbers and an operation code ('+', '-', '*', '/') to the server, and receives the result
+		- the request will be sent as a structure including the necessary fields, defined as following: 
+			struct operation{
+				int number1;	//value 1
+				int number2;	//value 2
+				char op;		//opcode - '+','-','*' or '/'
+			};
+
+		- the server receives the request structure, computes the result and returns it to the client as an integer
+*/
+
+/*
+	CLIENT
+*/
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <string.h>
+
+/* el código de error devuelto por ciertas llamadas */
+extern int errno;
+
+/* puerto de acceso al servidor */
+int port;
+
+int main (int argc, char *argv[]){
+	int sd;			// descriptor de socket
+	struct sockaddr_in server;	// la estructura utilizada para conectar
+	char msg[100];		// mensaje enviado
+
+	/* ¿Están todos los argumentos en la línea de comandos? */
+	if (argc != 3){
+      printf ("Sintaxis: %s <server_name> <port>\n", argv[0]);
+      return -1;
+    }
+
+	/* armamos el puerto */
+	port = atoi (argv[2]);
+
+	/* creación del socket */
+	if ((sd = socket (AF_INET, SOCK_STREAM, 0)) == -1){
+		perror ("Error de socket().\n");
+		return errno;
+    }
+
+	/* llenamos la estructura utilizada para conectar al servidor */
+	/* la familia de socket */
+	server.sin_family = AF_INET;
+	
+	/* dirección IP del servidor */
+	server.sin_addr.s_addr = inet_addr(argv[1]);
+	
+	/* el puerto de conexión */
+	server.sin_port = htons(port);
+
+	/* nos conectamos al servidor */
+	if (connect (sd, (struct sockaddr *) &server, sizeof(struct sockaddr)) == -1){
+		perror ("[client]Error en connect().\n");
+		return errno;
+    }
+
+	/* leyendo el mensaje */
+	bzero (msg, 100);
+	printf ("[client]Por favor ingrese un nombre: ");
+	fflush (stdout);
+	read (0, msg, 100);
+
+	/* enviando el mensaje al servidor */
+	if (write (sd, msg, 100) <= 0){
+		perror ("[client]Error al escribir en el servidor.\n");
+		return errno;
+    }
+
+	/* leyendo la respuesta del servidor (bloqueador de llamadas hasta que el servidor responda) */
+	if (read (sd, msg, 100) < 0){
+ 		perror ("[client]Error en read() del servidor.\n");
+		return errno;
+    }
+	
+	/* mostrar el mensaje recibido */
+	printf ("[client]El mensaje recibido es: %s\n", msg);
+
+	/* cerramos la conexión, hemos terminado */
+	close(sd);
+}
---
+++ Lab 6/ex1Server.c
@@ -0,0 +1,123 @@
+/*
+	Write a client and a TCP server following the steps described in the lab, where:
+		- the client sends two numbers and an operation code ('+', '-', '*', '/') to the server, and receives the result
+		- the request will be sent as a structure including the necessary fields, defined as following: 
+			struct operation{
+				int number1;	//value 1
+				int number2;	//value 2
+				char op;		//opcode - '+','-','*' or '/'
+			};
+
+		- the server receives the request structure, computes the result and returns it to the client as an integer
+*/
+
+/*
+	SERVER
+*/
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/* el puerto usado */
+#define PORT 2024
+
+/* el código de error devuelto por ciertas llamadas */
+extern int errno;
+
+int main (){
+	struct sockaddr_in server;	// la estructura utilizada por el servidor
+	struct sockaddr_in from;
+	char msg[100];		//mensaje recibido del cliente
+	char msgrasp[100]=" ";        //mensaje de respuesta del cliente
+	int sd;			//descriptor de socket
+
+	/* creando un socket */
+	if ((sd = socket (AF_INET, SOCK_STREAM, 0)) == -1){
+		perror ("[server]Error de socket().\n");
+		return errno;
+	}
+
+	/* preparación de estructuras de datos */
+	bzero (&server, sizeof (server));
+	bzero (&from, sizeof (from));
+
+	/* llene la estructura utilizada por el servidor */
+	/* estableciendo la familia de sockets */
+	server.sin_family = AF_INET;
+	
+	/* aceptamos cualquier dirección */
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	
+	/* usamos un puerto de usuario */
+	server.sin_port = htons(PORT);
+
+	/* adjuntamos el socket */
+	if (bind (sd, (struct sockaddr *) &server, sizeof (struct sockaddr)) == -1){
+		perror ("[server]Error en bind().\n");
+		return errno;
+	}
+
+	/* le pedimos al servidor que escuche si los clientes vienen a conectarse */
+	if (listen (sd, 5) == -1){
+		perror ("[server]Error de listen().\n");
+		return errno;
+	}
+
+	/* servimos a nuestros clientes iterativamente... */
+	while (1){
+		int client;
+		int length = sizeof(from);
+
+		printf ("[server]Estamos esperando el puerto %d...\n",PORT);
+		fflush (stdout);
+
+		/* aceptamos un cliente (estado de bloqueo hasta que se establezca la conexión) */
+		client = accept(sd, (struct sockaddr *) &from, &length);
+
+		/* error al aceptar una conexión de un cliente */
+		if (client < 0){
+			perror ("[server]Error en accept().\n");
+			continue;
+		}
+
+		/* se estableció la conexión, se espera el mensaje */
+		bzero (msg, 100);
+		printf ("[server]Esperamos el mensaje...\n");
+		fflush (stdout);
+
+		/* leyendo el mensaje */
+		if (read (client, msg, 100) <= 0){
+			perror ("[server]Error en read() del client.\n");
+			close (client);	/* cerramos la conexión con el cliente */
+			continue;		/* seguimos escuchando */
+		}
+
+		printf ("[server]Mensaje recibido...%s\n", msg);
+
+		/* preparamos el mensaje de respuesta */
+		bzero(msgrasp, 100);
+		strcat(msgrasp, "Hello ");
+		strcat(msgrasp, msg);
+
+		printf("[server]Enviamos el mensaje...%s\n", msgrasp);
+
+
+		/* devolvemos el mensaje del cliente */
+		if (write (client, msgrasp, 100) <= 0){
+			perror ("[server]Error en write() con client.\n");
+			continue;		/* seguimos escuchando */
+		}
+	  	else{
+			printf ("[server]Mensaje enviado con éxito.\n");
+		}
+		
+		/* terminamos con este cliente, cerramos la conexión */
+		close (client);
+	}				/* while */
+}				/* main */
